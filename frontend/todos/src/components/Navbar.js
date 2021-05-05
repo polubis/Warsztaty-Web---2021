3e8212819188f71deb1973087b0339dfe74491ae
polubis <polubis@wp.pl>
2021-05-05 14:07:17 +0100

    15. Stylizacja Navbar

--- frontend/todos/src/components/Navbar.js
+++ frontend/todos/src/components/Navbar.js
@@ -7,6 +7,8 @@ import {
   Tabs,
   Tab,
   Avatar,
+  makeStyles,
+  createStyles,
 } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import DashboardIcon from "@material-ui/icons/Dashboard";
@@ -14,36 +16,107 @@ import ListIcon from "@material-ui/icons/List";
 import ThemeIcon from "@material-ui/icons/Palette";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+
+      "& hr": {
+        height: 28,
+        margin: theme.spacing(0, 2.5),
+        background: theme.palette.common.white,
+      },
+    },
+
+    tab: {
+      display: "flex",
+      width: 130,
+      minWidth: "unset",
+      height: 64,
+      fontSize: theme.typography.fontSize,
+      textTransform: "none",
+
+      "&:hover": {
+        opacity: 1,
+      },
+
+      "& > span": {
+        flexDirection: "row",
+
+        "& svg": {
+          marginRight: theme.spacing(1),
+          transform: "translateY(2px)",
+        },
+      },
+    },
+
+    search: {
+      marginLeft: 200,
+      color: theme.palette.common.white,
+    },
+
+    theme: { marginLeft: "auto", color: theme.palette.common.white },
+
+    notifications: {
+      color: theme.palette.common.white,
+      marginLeft: theme.spacing(1),
+    },
+
+    email: {
+      margin: theme.spacing(0, 1.5, 0, 0.5),
+    },
+
+    avatar: {
+      background: theme.palette.common.white,
+      color: theme.palette.primary.main,
+    },
+  })
+);
+
 const Navbar = () => {
+  const classes = useStyles();
+
   return (
-    <AppBar>
+    <AppBar className={classes.root}>
       <Toolbar>
         <Typography variant="h6">Todo APP</Typography>
 
-        <IconButton>
+        <IconButton className={classes.search}>
           <SearchIcon />
         </IconButton>
 
         <Divider orientation="vertical" />
 
         <Tabs value={0}>
-          <Tab label="Dashboard" disableRipple icon={<DashboardIcon />} />
-          <Tab label="Tasks" disableRipple icon={<ListIcon />} />
+          <Tab
+            className={classes.tab}
+            label="Dashboard"
+            disableRipple
+            icon={<DashboardIcon />}
+          />
+          <Tab
+            className={classes.tab}
+            label="Tasks"
+            disableRipple
+            icon={<ListIcon />}
+          />
         </Tabs>
 
-        <IconButton>
+        <IconButton className={classes.theme}>
           <ThemeIcon />
         </IconButton>
 
-        <IconButton>
+        <IconButton className={classes.notifications}>
           <NotificationsIcon />
         </IconButton>
 
         <Divider orientation="vertical" />
 
-        <Typography variant="subtitle1">polubik1994@gmail.com</Typography>
+        <Typography className={classes.email} variant="subtitle1">
+          polubik1994@gmail.com
+        </Typography>
 
-        <Avatar>OP</Avatar>
+        <Avatar className={classes.avatar}>OP</Avatar>
       </Toolbar>
     </AppBar>
   );
