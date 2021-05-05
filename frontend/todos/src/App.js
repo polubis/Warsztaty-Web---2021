7b8ea9352dc1802c4e061cce6d4133cdd92e2bfb
polubis <polubis@wp.pl>
2021-05-05 13:10:09 +0100

    9. Omowienie struktury plikow i folderow oraz dodanie styli bazowych

--- frontend/todos/src/App.js
+++ frontend/todos/src/App.js
@@ -1,70 +1,13 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Text from "./Text";
-
-const PercentageText = (props) => {
-  return props.value <= 0 ? null : (
-    <span className={`percentage-text-${props.value > 100 ? "red" : "green"}`}>
-      {props.value} %
-    </span>
-  );
-};
-
-class UsersList extends React.Component {
-  state = {
-    users: [
-      { id: 0, name: "Piotr" },
-      { id: 1, name: "Piotr2" },
-      { id: 2, name: "Piotr3" },
-    ],
-  };
-
-  addUser = () => {
-    this.setState((prevState) => ({
-      users: [
-        ...prevState.users,
-        { id: prevState.users.length + 1, name: "Piotr4" },
-      ],
-    }));
-  };
-
-  render() {
-    return (
-      <ul onClick={this.addUser}>
-        {this.state.users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-    );
-  }
-}
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <UsersList />
-        <PercentageText value={15} />
-        <PercentageText value={0} />
-        <PercentageText value={101} />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Text>
-          You are about to <span>unban</span> polubis
-        </Text>
-      </header>
-    </div>
+    <div
+      style={{
+        width: "300px",
+        padding: "40px",
+        background: "red",
+        height: "300px",
+      }}
+    ></div>
   );
 }
 
--- frontend/todos/src/Text.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Text.css";
-
-const Text = (props) => {
-  return <h3 className="text">{props.children}</h3>;
-};
-
-export default Text;
