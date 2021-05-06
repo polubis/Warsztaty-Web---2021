b01e8a1cd09b516558343aad3afb98a9f69116f7
polubis <polubis@wp.pl>
2021-05-06 08:23:45 +0100

    22. Implementacja komponentu TasksStatesLoader

---
+++ frontend/todos/src/components/TasksStatesLoader.js
@@ -0,0 +1,32 @@
+import { Chip, makeStyles, createStyles, Box } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    loaders: {
+      display: "flex",
+    },
+
+    loader: {
+      background: theme.palette.grey[300],
+      width: 80,
+
+      "&:not(:last-of-type)": {
+        marginRight: 6,
+      },
+    },
+  })
+);
+
+const TasksStatesLoader = (props) => {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.loaders}>
+      {Array.from({ length: 5 }, (_, idx) => (
+        <Chip key={idx} className={classes.loader} />
+      ))}
+    </Box>
+  );
+};
+
+export default TasksStatesLoader;
--- frontend/todos/src/pages/TodosPage.js
+++ frontend/todos/src/pages/TodosPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import TasksLoader from "../components/TasksLoader";
+import TasksStatesLoader from "../components/TasksStatesLoader";
 
 class TodosPage extends React.Component {
   render() {
     return (
       <div>
+        <TasksStatesLoader />
         <TasksLoader />
       </div>
     );
