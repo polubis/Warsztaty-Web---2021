d3dd40abae787eff4d060035ea95cfbbc83b5024
polubis <polubis@wp.pl>
2021-05-06 10:13:33 +0100

    34. Dodanie stanu przejsciowego i pokazywania komponentow

--- frontend/todos/src/pages/TodosPage.js
+++ frontend/todos/src/pages/TodosPage.js
@@ -4,16 +4,31 @@ import TasksStatesLoader from "../components/TasksStatesLoader";
 import TasksPageHeader from "../components/TasksPageHeader";
 import TasksList from "../components/TasksList";
 import { Tasks } from "../mocks/Tasks";
+import { mockApiCall } from "../utils/mockApiCall";
 
 class TodosPage extends React.Component {
+  state = {
+    loading: true,
+  };
+
+  componentDidMount() {
+    mockApiCall(Tasks).then((tasks) => {
+      this.setState({ loading: false });
+    });
+  }
+
   render() {
+    const { loading } = this.state;
+
     return (
       <div>
+        <TasksPageHeader />
+
         {/* <TasksPageHeader />
         <TasksStatesLoader />
-        <TasksLoader /> */}
+        */}
 
-        <TasksList tasks={Tasks} />
+        {loading ? <TasksLoader /> : <TasksList tasks={Tasks} />}
       </div>
     );
   }
