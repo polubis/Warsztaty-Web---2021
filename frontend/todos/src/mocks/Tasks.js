d22fea9a58c0559217d160bb827a6c66e2f7a643
polubis <polubis@wp.pl>
2021-05-06 10:47:47 +0100

    41. Przygotowanie mockow pod dalsza prace

--- frontend/todos/src/mocks/Tasks.js
+++ frontend/todos/src/mocks/Tasks.js
@@ -1,3 +1,5 @@
+import { TasksStates } from "./TasksStates";
+
 export const Tasks = [
   {
     id: 0,
@@ -6,13 +8,7 @@ export const Tasks = [
     name: "Learn ASP.NET fundmantals",
     description:
       "Learn how to write web backend using ASP.NET .NET 5 framework.",
-    taskState: {
-      id: 2,
-      name: "To do",
-      description: "Task in this state means it is in progress",
-      fontColor: "#000000",
-      backgroundColor: "#FF0000",
-    },
+    taskState: TasksStates[0],
   },
   {
     id: 1,
@@ -20,12 +16,6 @@ export const Tasks = [
     modificationDate: "2019-04-01T00:00:00",
     name: "Wyniesc smieci",
     description: "",
-    taskState: {
-      id: 2,
-      name: "In Progress",
-      description: "Task in this state means it is in progress",
-      fontColor: "#555",
-      backgroundColor: "#ccc",
-    },
+    taskState: TasksStates[1],
   },
 ];
