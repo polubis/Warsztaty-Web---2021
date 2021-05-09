c0e304cbe4e82eaaa2e49ac307811f28399b4eac
polubis <polubis@wp.pl>
2021-05-09 09:11:22 +0100

    58. Dodanie walidacji

--- frontend/todos/src/components/TaskFormDialog.js
+++ frontend/todos/src/components/TaskFormDialog.js
@@ -12,25 +12,74 @@ import {
 } from "@material-ui/core";
 import { useState, useCallback } from "react";
 
+const validateName = (name) => {
+  if (name === "") {
+    return "Field name is required";
+  }
+
+  if (name.length < 3) {
+    return "Min characters  3";
+  }
+
+  if (name.length > 10) {
+    return "Max characters 10";
+  }
+
+  return "";
+};
+
+const validateDescription = (description) => {
+  if (description) {
+    if (description.length < 3) {
+      return "Min characters 3";
+    }
+
+    if (description.length > 10) {
+      return "Max characters 10";
+    }
+  }
+
+  return "";
+};
+
 const TaskFormDialog = () => {
   const [formData, setFormData] = useState({
     name: "",
     description: "",
   });
+  const [errors, setErrors] = useState({
+    name: "",
+    description: "",
+  });
 
   const handleNameChange = useCallback((e) => {
+    const { value } = e.target;
+
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      name: validateName(value),
+    }));
     setFormData((prevFormData) => ({
       ...prevFormData,
-      name: e.target.value,
+      name: value,
     }));
   }, []);
 
   const handeDescriptionChange = useCallback((e) => {
+    const { value } = e.target;
+
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      description: validateDescription(value),
+    }));
+
     setFormData((prevFormData) => ({
       ...prevFormData,
-      description: e.target.value,
+      description: value,
     }));
   }, []);
+
+  const formDataInvalid = Object.values(errors).some((error) => !!error);
 
   return (
     <Dialog open>
@@ -45,10 +94,14 @@ const TaskFormDialog = () => {
             name="name"
             variant="outlined"
             autoComplete="off"
+            aria-describedby="name-text"
             autoFocus
             value={formData.name}
             onChange={handleNameChange}
           />
+          {errors.name && (
+            <FormHelperText id="name-text">{errors.name}</FormHelperText>
+          )}
         </FormControl>
 
         <Box paddingTop={1}>
@@ -63,15 +116,23 @@ const TaskFormDialog = () => {
               value={formData.description}
               onChange={handeDescriptionChange}
             />
-            <FormHelperText id="description-text">
-              Will be needed later for better understanding purposes.
-            </FormHelperText>
+            {errors.description ? (
+              <FormHelperText id="description-text">
+                {errors.description}
+              </FormHelperText>
+            ) : (
+              <FormHelperText id="description-text">
+                Will be needed later for better understanding purposes.
+              </FormHelperText>
+            )}
           </FormControl>
         </Box>
       </DialogContent>
       <DialogActions>
         <Button color="primary">Cancel</Button>
-        <Button color="primary">Submit</Button>
+        <Button color="primary" disabled={formDataInvalid}>
+          Submit
+        </Button>
       </DialogActions>
     </Dialog>
   );
